594398d819590668ce6463a00b07101827ae2d29
Carlos Silva <carlosmms96@gmail.com>
2017-05-15 17:47:36 +0100

    ::::::

--- JSON/jsonReader/src/DAO_CONNECT.java
+++ JSON/jsonReader/src/DAO_CONNECT.java
@@ -3,18 +3,18 @@ import java.sql.*;
 public class DAO_CONNECT {   
 
       // Create a variable for the connection string.  
-      private static String connectionUrl = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databasename=Yum-Yum;;";
+      private static String connectionUrl = "jdbc:sqlserver://CARLOS:1433;databasename=Yum-Yum;";
       // Declare the JDBC objects.  
       public static Connection con = null;
       
-     // private static String user = "Carlos\\Carlos";
-     // private static String pass = "";
+      private static String user = "as";
+      private static String pass = "as";
       
       static{
             try {  
                // Establish the connection.  
                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");  
-               con = DriverManager.getConnection(connectionUrl);
+               con = DriverManager.getConnection(connectionUrl, user, pass);
             }  
 
             // Handle any errors that may have occurred.  
